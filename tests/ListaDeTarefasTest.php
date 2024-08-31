a7e53b2eedcfb2cf8fd1a07a58be7b2c2cbeee7d
Deborah Gonçalves Carrijo <100142069+deborahcarrijo@users.noreply.github.com>
2024-08-31 18:14:54 -0300

    Criação dos testes unitários para o projeto Lista de Tarefas.

---
+++ tests/ListaDeTarefasTest.php
@@ -0,0 +1,51 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class ListaDeTarefasTest extends TestCase
+{
+    private $listaDeTarefas;
+
+    protected function setUp(): void
+    {
+        $this->listaDeTarefas = new ListaDeTarefas();
+    }
+
+    public function testAdicionarTarefa()
+    {
+        $this->listaDeTarefas->adicionarTarefa('Tarefa 1');
+        $this->assertCount(1, $this->listaDeTarefas->obterTarefas());
+        $this->assertEquals('Tarefa 1', $this->listaDeTarefas->obterTarefas()[0]);
+    }
+
+    public function testAdicionarTarefaVazia()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('A tarefa não pode ser vazia.');
+        $this->listaDeTarefas->adicionarTarefa('');
+    }
+
+    public function testRemoverTarefa()
+    {
+        $this->listaDeTarefas->adicionarTarefa('Tarefa 1');
+        $this->listaDeTarefas->removerTarefa('Tarefa 1');
+        $this->assertCount(0, $this->listaDeTarefas->obterTarefas());
+    }
+
+    public function testRemoverTarefaInexistente()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Tarefa não encontrada.');
+        $this->listaDeTarefas->removerTarefa('Tarefa Inexistente');
+    }
+
+    public function testObterTarefas()
+    {
+        $this->listaDeTarefas->adicionarTarefa('Tarefa 1');
+        $this->listaDeTarefas->adicionarTarefa('Tarefa 2');
+        $tarefas = $this->listaDeTarefas->obterTarefas();
+        $this->assertCount(2, $tarefas);
+        $this->assertEquals('Tarefa 1', $tarefas[0]);
+        $this->assertEquals('Tarefa 2', $tarefas[1]);
+    }
+}
